e8a7486184f6fc3354cc06de3cf309eb14fb3175
Christine Eriksen <christine.eriksen@gmail.com>
2022-12-27 13:10:32 -0500

    commit tree e4432f63bce71ecaa2a7ea4cc1613b329e956301

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+// has a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement("h1");
+
+//the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.setAttribute("id","victory");
+
+// has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
+newHeader.textContent = "Christine is the champion";
